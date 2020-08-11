35debfa05b93f3394443eff6eec1357506b4aa9f
Cameron <hackwithcameron@gmail.com>
2020-08-11 16:13:23 -0700

    Comment Problem #2

--- Problem-2/problem-2.py
+++ Problem-2/problem-2.py
@@ -1,18 +1,23 @@
 import math
 
+# Test Arrays
 arry = [1, 2, 3, 4, 5]
 arry2 = [3, 2, 1]
 
 
 def product_of_nums(nums):
-    lst = []
-    for i in range(len(nums)):
-        num = nums[i]
-        nums.pop(i)
-        result = math.prod(nums)
-        lst.append(result)
-        nums.insert(i, num)
-    return lst
+    lst = []  # Create empty list
+
+    for i in range(len(nums)):  # Loop through input list using index value
+        num = nums[i]  # Sets num equal to the value of the number at index "i" in nums
+        nums.pop(i)  # Removes value at index "i"
+        result = math.prod(nums)  # Finds product of entire list with value at index "i" removed
+        lst.append(result)  # appends the result to the new list
+        nums.insert(i, num)  # re-insert the original number at index "i" back into place
+
+    return lst  # Return list once complete
 
 
 print(product_of_nums(arry))
+print(product_of_nums(arry2))
+
