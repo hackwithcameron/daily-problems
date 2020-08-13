7e8e16f6aaeb77e6951804a8aed7f7206f26664e
Cameron <hackwithcameron@gmail.com>
2020-08-12 18:41:28 -0700

    Comment Problem #3

--- Problem-3/Problem-3.py
+++ Problem-3/Problem-3.py
@@ -5,14 +5,15 @@ test_arry4 = [123, 12, 13, 14, 160, 16, -100, -14, -23]
 
 
 def find_first_missing_pos(arry):
-    arry.sort()
-    if arry[-1] > 0:
+    arry.sort()  # Sort Array from least to greatest
+    if arry[-1] > 0:  # Check if there are any positive numbers in array.
         for i in range(len(arry)):
-            if arry[i] >= 0:
+            if arry[i] >= 0:  # Check if number at index "i" in array is positive
                 try:
+                    # Check to see if next number in array is the next number numerically
                     if arry[i + 1] != arry[i] + 1:
-                        return print(arry[i] + 1)
-                except IndexError:
+                        return print(arry[i] + 1)  # If next number was not in numerical order return missing number
+                except IndexError:  # Catch index error and return the last indexed number in array + 1
                     return print(arry[i] + 1)
     else:
         return print("There are no positive numbers in the array.")
